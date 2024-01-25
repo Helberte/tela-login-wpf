af9710af53e8c25737096b9246881f91e2489635
Helberte Costa Arruda <helberteads@gmail.com>
2024-01-25 18:05:37 -0400

    commit inicial

---
+++ View/LoginView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppEstudoPromise.View
+{
+    /// <summary>
+    /// Lógica interna para LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        #region Eventos e Cliques
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // se o botão da esquerda do mouse estiver pressionado, DragMove() => arrasta a tela
+            if (e.LeftButton == MouseButtonState.Pressed)            
+                DragMove();
+            
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        #endregion
+
+
+    }
+}
