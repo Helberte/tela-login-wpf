a62663b68803d6de5ff82d7f3f1805c58949b342
Helberte Costa Arruda <helberteads@gmail.com>
2024-01-26 17:38:09 -0400

    Estudando elementos de tela e chamadas de layout

--- View/LoginView.xaml.cs
+++ View/LoginView.xaml.cs
@@ -22,6 +22,29 @@ namespace AppEstudoPromise.View
         public LoginView()
         {
             InitializeComponent();
+
+            OtimizacaoLayout();
+        }
+
+        private void OtimizacaoLayout()
+        {
+            txtUsuario.Focus();
+
+            // Verifica se é possível tornar o objeto Brush freeze => congelado
+            // isso ajuda a melhorar o desempenho visto que o sistema vai gastar menos recursos
+            // monitorando suas notificações de alterações
+            /*
+            if (borderInterna.BorderBrush.CanFreeze) { borderInterna.BorderBrush.Freeze(); }
+            if (gradienteBordaInterna.CanFreeze)     { gradienteBordaInterna.Freeze(); }
+            */
+            /*             
+            Nem todo objeto Freezable pode ser congelado. Para evitar lançar um InvalidOperationException, 
+            verifique o valor da propriedade CanFreeze do objeto Freezable para determinar se ele pode 
+            ser congelado antes de tentar congelá-lo.             
+            */
+
+            // saber se está congelado
+            //if (gradienteBordaInterna.IsFrozen ) { }
         }
 
         #region Eventos e Cliques
@@ -30,8 +53,7 @@ namespace AppEstudoPromise.View
         {
             // se o botão da esquerda do mouse estiver pressionado, DragMove() => arrasta a tela
             if (e.LeftButton == MouseButtonState.Pressed)            
-                DragMove();
-            
+                DragMove();            
         }
 
         private void btnMinimize_Click(object sender, RoutedEventArgs e)
@@ -49,6 +71,15 @@ namespace AppEstudoPromise.View
 
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            EsqueciSenha senha = new EsqueciSenha();
+            senha.Show();
+
+            this.Close();
+        }
+
         #endregion
+
     }
 }
